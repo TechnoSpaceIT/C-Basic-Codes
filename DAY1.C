e55b495dac5aae602333953f421d5be585a59ab3
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-01-31 20:37:18 +0530

    [ADDED]:[Tahir] Day1 Demo for Printf

---
+++ DAY1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("Hello Computer");
+	printf("\n\tGood Bye");
+	printf("\n*********");
+	printf("\n*\t*");
+	printf("\n*\t*");
+	printf("\n*********");
+	printf("\n*\t*");
+	printf("\n*\t*");
+	getch();
+}
