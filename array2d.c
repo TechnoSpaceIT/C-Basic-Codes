c0d1628475fa5b1fa339636c86381fca528b10e3
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-02-16 21:24:24 +0530

    [ADDED]:[Tahir] Program for 2D Array with Matrix Demo in C

---
+++ array2d.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],r,c;
+	printf("\nEnter the 3x3 Matrix");
+	
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			scanf("%d",&a[r][c]);
+		}
+	}
+	
+	printf("\nThe 3x3 Matrix is:\n");
+	
+	for(r=0;r<=2;r++)
+	{
+		for(c=0;c<=2;c++)
+		{
+			printf("\t%d",a[r][c]);
+		}
+		printf("\n");
+	}
+}
